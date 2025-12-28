2653796a515fa1ebb2a8b58be81fdac35f676b1c
ygt-ernsy <yigiterensoy@hotmail.com>
2025-12-29 00:27:52 +0300

    feat: Added an aditional method for the interface TokenBlacklistService for it to allow setting the blacklist duration manually

--- src/main/java/com/iyte_yazilim/proje_pazari/domain/security/TokenBlacklistService.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/domain/security/TokenBlacklistService.java
@@ -1,5 +1,7 @@
 package com.iyte_yazilim.proje_pazari.domain.security;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
+
 /**
  * TokenBlacklistService
  */
@@ -7,4 +9,6 @@ public interface TokenBlacklistService {
     void blacklistToken(String token);
 
     boolean isTokenBlacklisted(String token);
+
+    void blacklistToken(String token, Duration ttl);
 }
