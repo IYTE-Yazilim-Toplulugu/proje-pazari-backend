3b40b2404537be726c75d29c8971c6793016b64d
ygt-ernsy <yigiterensoy@hotmail.com>
2025-12-28 14:46:15 +0300

    fix: Refactored the UserSearchRepository to be in the correct package

--- src/main/java/com/iyte_yazilim/proje_pazari/infrastructure/persistence/UserSearchRepository.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/infrastructure/persistence/UserSearchRepository.java
@@ -1,5 +1,6 @@
-package com.iyte_yazilim.proje_pazari.infrastructure.persistence.models;
+package com.iyte_yazilim.proje_pazari.infrastructure.persistence;
 
+import com.iyte_yazilim.proje_pazari.infrastructure.persistence.models.UserDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
