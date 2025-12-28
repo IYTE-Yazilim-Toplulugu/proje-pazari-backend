ea426da988f411ac5f641602cc809cc7077bb2df
AliKemalMiloglu <alikemalmiloglu@gmail.com>
2025-12-28 14:39:29 +0300

    feat: Created UserDocument model for indexing users.

---
+++ src/main/java/com/iyte_yazilim/proje_pazari/infrastructure/persistence/models/UserDocument.java
@@ -0,0 +1,46 @@
+package com.iyte_yazilim.proje_pazari.infrastructure.persistence.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Document(indexName = "#{@environment.getProperty('app.elasticsearch.user-index')}")
+public class UserDocument {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private String id;
+
+    @Field(type = FieldType.Text, analyzer = "standard")
+    private String firstName;
+
+    @Field(type = FieldType.Text, analyzer = "standard")
+    private String lastName;
+
+    @Field(type = FieldType.Text, analyzer = "standard")
+    private String fullName;
+
+    @Field(type = FieldType.Keyword)
+    private String email;
+
+    @Field(type = FieldType.Text, analyzer = "standard")
+    private String description;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_time)
+    private LocalDateTime joinedAt;
+}
