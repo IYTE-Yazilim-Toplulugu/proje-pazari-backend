22821fad0d5fe7f31cb66dfd5cd04ff1fd32659e
ygt-ernsy <yigiterensoy@hotmail.com>
2025-12-29 13:31:06 +0300

    feat: Added extra fields to the event

--- src/main/java/com/iyte_yazilim/proje_pazari/domain/events/ApplicationSubmittedEvent.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/domain/events/ApplicationSubmittedEvent.java
@@ -3,6 +3,10 @@ package com.iyte_yazilim.proje_pazari.domain.events;
 public record ApplicationSubmittedEvent(
         String applicationId,
         String projectId,
+        String projectTitle,
+        String applicantId,
         String applicantEmail,
-        String projectTitle
+        String applicantFirstName,
+        String ownerEmail,
+        String ownerFirstName
 ) implements IDomainEvent {}
