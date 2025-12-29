cea60616822762e216de89da240aac96502f20c2
ygt-ernsy <yigiterensoy@hotmail.com>
2025-12-29 19:40:18 +0300

    fix: Fixed the formatting of the anotaions in RegisterUserCommand

--- src/main/java/com/iyte_yazilim/proje_pazari/application/commands/registerUser/RegisterUserCommand.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/application/commands/registerUser/RegisterUserCommand.java
@@ -83,10 +83,12 @@ public record RegisterUserCommand(
                         regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
                         message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character")
                 String password,
+
         @Schema(description = "First name", example = "John")
                 @NotBlank(message = "First name is required")
                 @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
                 String firstName,
+
         @Schema(description = "Last name", example = "Doe")
                 @NotBlank(message = "Last name is required")
                 @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
