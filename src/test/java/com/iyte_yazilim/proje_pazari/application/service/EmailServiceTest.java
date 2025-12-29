e23183af25223197c1fc0c1ed64b95ec264c6232
ygt-ernsy <yigiterensoy@hotmail.com>
2025-12-29 15:11:15 +0300

    test(email): Added test for email service. This test was created in a new package since the package already present was faulty

--- src/test/java/com/iyte_yazilim/proje_pazari/application/service/EmailServiceTest.java
+++ src/test/java/com/iyte_yazilim/proje_pazari/application/service/EmailServiceTest.java
@@ -1,26 +1,44 @@
 package com.iyte_yazilim.proje_pazari.application.service;
 
-import com.icegreen.greenmail.util.GreenMail;
-import com.icegreen.greenmail.spring.GreenMailBean;
-import jakarta.mail.internet.MimeMessage;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import com.icegreen.greenmail.configuration.GreenMailConfiguration;
 import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import com.iyte_yazilim.proje_pazari.application.dtos.EmailDto;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class EmailServiceTest {
 
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
+            .withConfiguration(GreenMailConfiguration.aConfig().withUser("test", "test"));
+
+    @DynamicPropertySource
+    static void configureMailProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.mail.host", () -> "localhost");
+        registry.add("spring.mail.port", () -> ServerSetupTest.SMTP.getPort());
+        registry.add("spring.mail.username", () -> "test");
+        registry.add("spring.mail.password", () -> "test");
+    }
+
     @Autowired
-    private GreenMail greenMail;
+    private EmailService emailService;
 
     @Test
     void shouldSendWelcomeEmail() {
-        // Test email sending
+        // Send an email
+        EmailDto emailDto = new EmailDto("recipient@example.com", "Test Subject", "Test Body");
+        emailService.sendEmail(emailDto);
+
+        // Verify email was received
         MimeMessage[] receivedMessages = greenMail.getReceivedMessages();
         assertEquals(1, receivedMessages.length);
     }
