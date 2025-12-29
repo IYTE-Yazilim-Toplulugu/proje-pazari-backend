04660dc0c79ca95447e5f7c0db01f2f387172394
ygt-ernsy <yigiterensoy@hotmail.com>
2025-12-29 13:50:11 +0300

    feat: Added extra necesary fields to the event

--- src/main/java/com/iyte_yazilim/proje_pazari/domain/events/ApplicationReviewedEvent.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/domain/events/ApplicationReviewedEvent.java
@@ -6,6 +6,9 @@ public record ApplicationReviewedEvent(
         String applicationId,
         String applicantEmail,
         String projectTitle,
+        String applicantFirstName,
+        String ownerFirstName,
+        String ownerEmail,
         ApplicationStatus status,
-        String reviewMessage
-) implements IDomainEvent {}
+        String reviewMessage) implements IDomainEvent {
+}
