83c496a9891fb11d0ce53843a051747d832204f4
ygt-ernsy <yigiterensoy@hotmail.com>
2026-01-02 16:45:54 +0300

    feat: Created ElasticesearchEventListener

---
+++ src/main/java/com/iyte_yazilim/proje_pazari/infrastructure/persistence/elasticsearch/ElasticsearchEventListener.java
@@ -0,0 +1,40 @@
+package com.iyte_yazilim.proje_pazari.infrastructure.persistence.elasticsearch;
+
+import com.iyte_yazilim.proje_pazari.domain.events.ProjectCreatedEvent;
+import com.iyte_yazilim.proje_pazari.domain.events.ProjectDeletedEvent;
+import com.iyte_yazilim.proje_pazari.domain.events.ProjectUpdatedEvent;
+import com.iyte_yazilim.proje_pazari.infrastructure.persistence.elasticsearch.services.ElasticsearchSyncService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ElasticsearchEventListener {
+
+    private final ElasticsearchSyncService syncService;
+
+    @EventListener
+    @Async
+    public void handleProjectCreated(ProjectCreatedEvent event) {
+        log.debug("Indexing newly created project: {}", event.projectId());
+        syncService.indexProject(event.projectId());
+    }
+
+    @EventListener
+    @Async
+    public void handleProjectUpdated(ProjectUpdatedEvent event) {
+        log.debug("Re-indexing updated project: {}", event.projectId());
+        syncService.indexProject(event.projectId());
+    }
+
+    @EventListener
+    @Async
+    public void handleProjectDeleted(ProjectDeletedEvent event) {
+        log.debug("Removing deleted project from index: {}", event.projectId());
+        syncService.deleteProjectIndex(event.projectId());
+    }
+}
