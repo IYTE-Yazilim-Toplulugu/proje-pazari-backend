b9a8cec8e08ac18737eed6d5d38d8021cfea18c6
ygt-ernsy <yigiterensoy@hotmail.com>
2026-01-16 20:07:41 +0300

    fix: the wrong import that copilot pointed out in the pr comments wasn't wrong

--- src/test/java/com/iyte_yazilim/proje_pazari/presentation/controllers/SearchControllerTest.java
+++ src/test/java/com/iyte_yazilim/proje_pazari/presentation/controllers/SearchControllerTest.java
@@ -25,7 +25,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHitSupport;
