b34c4c4819e26633a529b4804796ee75cfb73122
Yusuf-sudo-it <yusuf.2004.381@gmail.com>
2026-01-18 17:54:09 +0300

    feat : - Added Role and RoleType entities .

---
+++ src/main/java/com/iyte_yazilim/proje_pazari/domain/entities/Role.java
@@ -0,0 +1,26 @@
+package com.iyte_yazilim.proje_pazari.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role extends BaseEntity<String> {
+
+    @Column(unique = true, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RoleType name;
+    
+    private String description;
+}
