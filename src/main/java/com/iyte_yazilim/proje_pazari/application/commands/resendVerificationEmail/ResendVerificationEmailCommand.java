81f385151ff071005f56b6ca525cc6c04465b9de
neonid0 <yunusbozcan@std.iyte.edu.tr>
2026-02-09 22:11:18 +0300

    refactor: improve email verification command structure

--- src/main/java/com/iyte_yazilim/proje_pazari/application/commands/resendVerificationEmail/ResendVerificationEmailCommand.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/application/commands/resendVerificationEmail/ResendVerificationEmailCommand.java
@@ -1,3 +1,6 @@
 package com.iyte_yazilim.proje_pazari.application.commands.resendVerificationEmail;
 
-public record ResendVerificationEmailCommand(String email) {}
+import com.iyte_yazilim.proje_pazari.application.common.IRequest;
+import com.iyte_yazilim.proje_pazari.domain.models.ApiResponse;
+
+public record ResendVerificationEmailCommand(String email) implements IRequest<ApiResponse<Void>> {}
