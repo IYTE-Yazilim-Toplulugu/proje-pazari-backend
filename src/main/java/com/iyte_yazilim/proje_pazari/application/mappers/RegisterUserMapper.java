2259f4ec30b287da4d09f4fe1f0e9d24910c2303
neonid0 <yunusbozcan@std.iyte.edu.tr>
2026-02-10 23:27:09 +0300

    fix: remove non-existent email verification field mappings from RegisterUserMapper
    - Remove mappings for emailVerified, verificationToken, verificationTokenExpiresAt, emailVerifiedAt
    - These fields were removed from User entity but mapping annotations were not cleaned up
    - Fixes compilation errors in MapStruct mapper generation

--- src/main/java/com/iyte_yazilim/proje_pazari/application/mappers/RegisterUserMapper.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/application/mappers/RegisterUserMapper.java
@@ -43,10 +43,6 @@ public interface RegisterUserMapper {
     @Mapping(target = "githubUrl", ignore = true)
     @Mapping(target = "active", ignore = true)
     @Mapping(target = "roles", ignore = true)
-    @Mapping(target = "emailVerified", ignore = true)
-    @Mapping(target = "verificationToken", ignore = true)
-    @Mapping(target = "verificationTokenExpiresAt", ignore = true)
-    @Mapping(target = "emailVerifiedAt", ignore = true)
     User commandToDomain(RegisterUserCommand command);
 
     /**
