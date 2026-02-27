feb338209c3afd0343642353a89d484ac0d299a0
Barkın <bdurgut06@gmail.com>
2026-02-27 12:39:06 +0300

    fix: removed duplicate search command and fixed missing interface annotations

--- src/main/java/com/iyte_yazilim/proje_pazari/application/queries/getAllProjects/GetAllProjectsQuery.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/application/queries/getAllProjects/GetAllProjectsQuery.java
@@ -1,3 +1,9 @@
 package com.iyte_yazilim.proje_pazari.application.queries.getAllProjects;
 
-public record GetAllProjectsQuery(int page, int size, String sortBy, String sortDirection) {}
+import com.iyte_yazilim.proje_pazari.application.common.IRequest;
+import com.iyte_yazilim.proje_pazari.domain.models.ApiResponse;
+import com.iyte_yazilim.proje_pazari.domain.models.results.PagedProjectsResult;
+
+public record GetAllProjectsQuery(int page, int size, String sortBy, String sortDirection)
+        implements IRequest<ApiResponse<PagedProjectsResult>> {
+}
--- src/main/java/com/iyte_yazilim/proje_pazari/application/queries/getProject/GetProjectQuery.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/application/queries/getProject/GetProjectQuery.java
@@ -1,4 +1,9 @@
 package com.iyte_yazilim.proje_pazari.application.queries.getProject;
 
+import com.iyte_yazilim.proje_pazari.application.common.IRequest;
+import com.iyte_yazilim.proje_pazari.application.dtos.ProjectDetailDto;
+import com.iyte_yazilim.proje_pazari.domain.models.ApiResponse;
+
 /** GetProjectQuery */
-public record GetProjectQuery(String projectId) {}
+public record GetProjectQuery(String projectId) implements IRequest<ApiResponse<ProjectDetailDto>> {
+}
