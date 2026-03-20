3eea3ea21973217bf44ceb01b1ca02bc70196bb8
Efe Tamtürk <efetamturk@gmail.com>
2026-03-20 22:45:22 +0000

    format: ElasticsearchEventListener.java

--- src/main/java/com/iyte_yazilim/proje_pazari/infrastructure/persistence/elasticsearch/ElasticsearchEventListener.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/infrastructure/persistence/elasticsearch/ElasticsearchEventListener.java
@@ -9,9 +9,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
-import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
