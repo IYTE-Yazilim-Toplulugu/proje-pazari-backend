cc6d65454621da516b3e38410ab9b1849703b312
ygt-ernsy <yigiterensoy@hotmail.com>
2026-03-24 01:37:07 +0300

    test(admin): add missing ApplicationEventPublisher mock to AdminDeleteUserHandlerTes

--- src/test/java/com/iyte_yazilim/proje_pazari/application/commands/adminDeleteUser/AdminDeleteUserHandlerTest.java
+++ src/test/java/com/iyte_yazilim/proje_pazari/application/commands/adminDeleteUser/AdminDeleteUserHandlerTest.java
@@ -17,11 +17,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 
 @ExtendWith(MockitoExtension.class)
 class AdminDeleteUserHandlerTest {
 
     @Mock private UserRepository userRepository;
+
+    @Mock private ApplicationEventPublisher applicationEventPublisher;
 
     @InjectMocks private AdminDeleteUserHandler handler;
 
