6f6dd9cc2a1a9277e0f32597ca189cd7d2dfd68a
ygt-ernsy <yigiterensoy@hotmail.com>
2026-03-23 19:51:45 +0300

    fix: fixed typo turkhis_search → turkish_search in ProjectDocument.java

--- src/main/java/com/iyte_yazilim/proje_pazari/infrastructure/persistence/models/ProjectDocument.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/infrastructure/persistence/models/ProjectDocument.java
@@ -26,10 +26,10 @@ public class ProjectDocument {
 
     @Id private String id;
 
-    @Field(type = FieldType.Text, analyzer = "turkhis_search")
+    @Field(type = FieldType.Text, analyzer = "turkish_search")
     private String title;
 
-    @Field(type = FieldType.Text, analyzer = "turkhis_search")
+    @Field(type = FieldType.Text, analyzer = "turkish_search")
     private String description;
 
     @Field(type = FieldType.Text)
