21aa1b09e4ee6a19a4c8052987db5e9360bf4fd3
Deniz1923 <ozgurdeniz2018@example.com>
2026-03-24 09:04:17 +0300

    fix: forgor

--- src/main/java/com/iyte_yazilim/proje_pazari/presentation/controllers/AuthController.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/presentation/controllers/AuthController.java
@@ -25,6 +25,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -296,6 +297,7 @@ public class AuthController extends BaseController {
     }
 
     @PostMapping("/logout")
+    @PreAuthorize("isAuthenticated()")
     @SecurityRequirement(name = "Bearer Authentication")
     @Operation(
             summary = "Logout user",
