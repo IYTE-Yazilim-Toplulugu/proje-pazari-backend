4d0ffa10b791b7b17f6c8ddaa3f08dc4024866aa
ygt-ernsy <yigiterensoy@hotmail.com>
2026-04-06 21:26:32 +0300

    fix: removed the unused import

--- src/main/java/com/iyte_yazilim/proje_pazari/presentation/controllers/ProjectController.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/presentation/controllers/ProjectController.java
@@ -9,7 +9,6 @@ import com.iyte_yazilim.proje_pazari.application.dtos.PagedProjectsResult;
 import com.iyte_yazilim.proje_pazari.application.dtos.ProjectDetailDto;
 import com.iyte_yazilim.proje_pazari.application.queries.getAllProjects.GetAllProjectsQuery;
 import com.iyte_yazilim.proje_pazari.application.queries.getProject.GetProjectQuery;
-import com.iyte_yazilim.proje_pazari.application.queries.getUserProjects.GetUserProjectsQuery;
 import com.iyte_yazilim.proje_pazari.domain.models.results.CreateProjectCommandResult;
 import com.iyte_yazilim.proje_pazari.domain.models.results.UpdateProjectStatusCommandResult;
 import io.swagger.v3.oas.annotations.Operation;
