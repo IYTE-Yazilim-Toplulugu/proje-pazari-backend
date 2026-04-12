0fdf3106b4d7bd6501a5c76ee6d3ad3e0399b6e8
Efe Tamtürk <efetamturk@gmail.com>
2026-04-13 01:22:16 +0300

    fix: changed all command transaction gaps IRequest to ICommand

--- src/main/java/com/iyte_yazilim/proje_pazari/application/commands/bulkApplicationAction/BulkApplicationActionCommand.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/application/commands/bulkApplicationAction/BulkApplicationActionCommand.java
@@ -1,9 +1,9 @@
 package com.iyte_yazilim.proje_pazari.application.commands.bulkApplicationAction;
 
-import com.iyte_yazilim.proje_pazari.application.common.IRequest;
+import com.iyte_yazilim.proje_pazari.application.common.ICommand;
 import com.iyte_yazilim.proje_pazari.application.dtos.BulkActionResult;
 import com.iyte_yazilim.proje_pazari.domain.models.ApiResponse;
 import java.util.List;
 
 public record BulkApplicationActionCommand(String action, List<String> applicationIds)
-        implements IRequest<ApiResponse<BulkActionResult>> {}
+        implements ICommand<ApiResponse<BulkActionResult>> {}
--- src/main/java/com/iyte_yazilim/proje_pazari/application/commands/refreshToken/RefreshTokenCommand.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/application/commands/refreshToken/RefreshTokenCommand.java
@@ -1,7 +1,7 @@
 package com.iyte_yazilim.proje_pazari.application.commands.refreshToken;
 
-import com.iyte_yazilim.proje_pazari.application.common.IRequest;
+import com.iyte_yazilim.proje_pazari.application.common.ICommand;
 import com.iyte_yazilim.proje_pazari.domain.models.ApiResponse;
 
 public record RefreshTokenCommand(String refreshToken)
-        implements IRequest<ApiResponse<RefreshTokenResult>> {}
+        implements ICommand<ApiResponse<RefreshTokenResult>> {}
