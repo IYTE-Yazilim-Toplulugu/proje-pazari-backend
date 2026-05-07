2ae63122999f959293eeb414e4ab5f873a3900a6
Talha <talhakarakaya101010@gmail.com>
2026-05-08 00:30:54 +0300

    refactor(application): extract password validation and CSV parsing from handlers

--- src/main/java/com/iyte_yazilim/proje_pazari/application/commands/changePassword/ChangePasswordCommand.java
+++ src/main/java/com/iyte_yazilim/proje_pazari/application/commands/changePassword/ChangePasswordCommand.java
@@ -2,11 +2,14 @@ package com.iyte_yazilim.proje_pazari.application.commands.changePassword;
 
 import com.iyte_yazilim.proje_pazari.application.common.ApiResponse;
 import com.iyte_yazilim.proje_pazari.application.common.ICommand;
+import com.iyte_yazilim.proje_pazari.application.validators.PasswordMatches;
+import com.iyte_yazilim.proje_pazari.application.validators.ValidPassword;
+import com.iyte_yazilim.proje_pazari.domain.interfaces.PasswordMatchable;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 @Schema(description = "Command to change user password")
+@PasswordMatches
 public record ChangePasswordCommand(
         @Schema(description = "User ID", requiredMode = Schema.RequiredMode.REQUIRED)
                 @NotBlank(message = "User ID is required")
@@ -16,19 +19,9 @@ public record ChangePasswordCommand(
                 String currentPassword,
         @Schema(description = "New password", requiredMode = Schema.RequiredMode.REQUIRED)
                 @NotBlank(message = "New password is required")
-                @Size(min = 8, message = "New password must be at least 8 characters")
+                @ValidPassword
                 String newPassword,
         @Schema(description = "Confirm new password", requiredMode = Schema.RequiredMode.REQUIRED)
                 @NotBlank(message = "Password confirmation is required")
                 String confirmPassword)
-        implements ICommand<ApiResponse<Void>> {
-    public void validate() {
-        if (!newPassword.equals(confirmPassword)) {
-            throw new IllegalArgumentException("New password and confirmation do not match");
-        }
-        if (currentPassword.equals(newPassword)) {
-            throw new IllegalArgumentException(
-                    "New password must be different from current password");
-        }
-    }
-}
+        implements ICommand<ApiResponse<Void>>, PasswordMatchable {}
